f3bc4047764ca5010457cd22d463ed88fc569ec5
KevinAraujo12 <kevinnunes20011210@gmail.com>
2024-03-17 16:36:04 -0300

    🚀:Criado o nosso Header

--- projeto-filme/src/routes.js
+++ projeto-filme/src/routes.js
@@ -2,10 +2,12 @@ import { BrowserRouter, Routes, Route } from "react-router-dom";
 
 import Filme from "./pages/Filme";
 import Home from "./pages/Home";
+import Header from "./components/Header";
 
 function RouteApp(){
     return(
         <BrowserRouter>
+            <Header/>
             <Routes>
                 <Route path="/" element={  <Home/> }></Route>
                 <Route path="/filme:id" element={  <Filme/> }></Route>
